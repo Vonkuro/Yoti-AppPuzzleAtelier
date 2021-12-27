ffc14e03f21770af2760d7c07af4dbc3f49316f0
vonkuro <vonkuro@gmail.com>
2021-12-27 11:29:51 +0100

    projet set up remote

---
+++ mainWidget.cpp
@@ -0,0 +1,14 @@
+#include "mainWidget.h"
+#include "ui_mainWidget.h"
+
+MainWidget::MainWidget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::MainWidget)
+{
+    ui->setupUi(this);
+}
+
+MainWidget::~MainWidget()
+{
+    delete ui;
+}
