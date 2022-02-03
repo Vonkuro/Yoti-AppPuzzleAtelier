6d5e1bf1a4b70830feb789f3bcb8d15bf20fd326
vonkuro <vonkuro@gmail.com>
2022-02-03 09:34:35 +0100

    loader exist

---
+++ AppWork/loader.cpp
@@ -0,0 +1,12 @@
+#include "loader.h"
+
+Loader::Loader(QObject *parent) : QObject(parent)
+{
+
+}
+
+// trouver les id des puzzles non handled
+// preparer le path vers le dossier image
+// preparer le path vers la cover
+
+
